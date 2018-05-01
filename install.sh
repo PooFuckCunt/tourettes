25dca4432c7c418bd639edd3fe25da1a995af1a0
PooFuckCunt <kasperjones.1989@gmail.com>
2018-05-01 15:25:11 +1000

    First commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo cp -a `pwd` /usr/local/share
+sudo cp tourettes /usr/bin
+sudo chmod a+x /usr/bin/tourettes 
---
+++ randmp3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#number of files to keep put one more than desired number e.g 11 for total of 10
+keepX=11
+#mp3 player
+PLAYER="mpv"
+
+
+
+one=`sort -R list.txt |head -1`
+two=`sort -R list.txt |head -1`
+three=`sort -R list.txt |head -1`
+one1=`sort -R list.txt |head -1`
+two1=`sort -R list.txt |head -1`
+three1=`sort -R list.txt |head -1`
+#randome string for filename
+NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+#random number for mp3 list
+rand=$(( ( RANDOM % 65 )  + 2 ))
+#create dir 
+mkdir -p /tmp/tourettes
+#create random mp3
+cat `ls swearwords/*.mp3 |shuf |sort -R |shuf |sort -R |head -n$rand` > /tmp/tourettes/tourettes.0.mp3
+#reencode mp3
+ffmpeg -y -i /tmp/tourettes/tourettes.0.mp3 -metadata title="$one1 $two1 $three1" -metadata artist="$one $two $three" -vn -ar 44100 -ac 2 -ab 48k -f mp3 /tmp/tourettes/tourettes.$NAME.mp3 > /dev/null 2>&1
+#clean up old mp3s
+rm /tmp/tourettes/tourettes.0.mp3
+cd /tmp/tourettes && ls -tp | grep -v '/$' | tail -n +$keepX | xargs -d '\n' -r rm --
+$PLAYER /tmp/tourettes/tourettes.$NAME.mp3
+#ffmpeg -i /tmp/tourettes/tourettes.mp3 -loop 1 -i ~/tourettes/images/04.png -shortest /tmp/out.avi
