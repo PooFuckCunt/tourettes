b0b8e6f6470e26de71c9d877a553c015dcb9ff12
PooFuckCunt <38875366+PooFuckCunt@users.noreply.github.com>
2018-05-02 20:43:48 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp -a `pwd` /usr/local/share
+sudo mkdir -p /usr/local/share/tourettes
+sudo cp -a `pwd`/* /usr/local/share/tourettes
 sudo cp tourettes /usr/bin
-sudo chmod a+x /usr/bin/tourettes 
-sudo rm -rf /usr/local/share/tourettes/.git
+sudo chmod a+x /usr/bin/tourettes
