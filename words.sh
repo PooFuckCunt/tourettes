a8df12f0780d169b2db8b599e244487d80582dfe
KB_tourettes <38875366+PooFuckCunt@users.noreply.github.com>
2019-10-08 22:40:21 +1100

    Added specific word output
    e.g you can now type "tourettes I am a wanker" and it will say it verbatim if a word is not found it will say so and exit cleanly

---
+++ words.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+FILES=/tmp/tourettes/.swords
+SW_PATH=/usr/local/share/tourettes/swearwords
+WORDS=/usr/local/share/tourettes/words
+one=`sort -R list.txt |head -1`
+two=`sort -R list.txt |head -1`
+three=`sort -R list.txt |head -1`
+one1=`sort -R list.txt |head -1`
+two1=`sort -R list.txt |head -1`
+three1=`sort -R list.txt |head -1`
+PLAYER=`grep 'PLAYER' config | sed -e 's#.*=\(\)#\1#'`
+
+find $SW_PATH -iname '*-*' > $FILES
+find $WORDS -iname '*-*' >> $FILES
+for words in $@
+do
+if grep -q "/$words-" $FILES; then
+echo 'poo fart' >> /dev/null 
+else
+    echo "$words not found" ; exit
+fi
+done
+for words in $@
+do
+ cat `grep "/$words-"  $FILES |head -n1` >> /tmp/tourettes/phrase.0.mp3
+done
+
+ffmpeg -y -i /tmp/tourettes/phrase.0.mp3 -metadata title="$one1 $two1 $three1" -metadata artist="$one $two $three" -vn -ar 44100 -ac 2 -ab 48k -f mp3 /tmp/tourettes/phrase.mp3 > /dev/null 2>&1
+
+rm /tmp/tourettes/phrase.0.mp3
+$PLAYER /tmp/tourettes/phrase.mp3
