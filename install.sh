0e13e4e13053bc6c4bd2213620facae1ebc9df7b
KB_tourettes <kasperjones.1989@gmail.com>
2019-10-09 14:52:39 +1100

    changed some file names added some new ones edited the install file

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo mkdir -p /usr/local/share/tourettes
-sudo cp -a `pwd`/* /usr/local/share/tourettes
+mkdir -p /usr/local/share/tourettes
+cp -a `pwd`/* /usr/local/share/tourettes
 sudo cp tourettes /usr/bin
 sudo chmod a+x /usr/bin/tourettes
+sudo chmod a+x /usr/local/share/tourettes/words.sh
+sudo chmod a+x /usr/local/share/tourettes/randmp3.sh
